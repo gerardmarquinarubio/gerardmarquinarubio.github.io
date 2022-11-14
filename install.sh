9f2885d761055106de7eb948637fc005664a0101
Gerard Marquina Rubio <gerardmarquinarubio@gmail.com>
2022-11-14 22:17:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+curl https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash 
 brew install wget node yarn deno
 brew install --cask rectangle alt-tab google-chrome visual-studio-code firefox
 curl https://bun.sh/install | bash
