0d26e2b829741172bcd5d6d912c35b2f1322dfe8
Gerard Marquina Rubio <gerardmarquinarubio@gmail.com>
2022-11-15 13:16:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-curl https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash 
-brew install wget node yarn deno
-brew install --cask rectangle alt-tab google-chrome visual-studio-code firefox
-curl https://bun.sh/install | bash
+curl https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash && brew install wget node yarn deno && brew install --cask rectangle alt-tab google-chrome visual-studio-code firefox && curl https://bun.sh/install | bash && curl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
