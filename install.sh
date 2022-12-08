df605d3004c854b97f9bd58e9b2c016edf7acd4b
Gerard Marquina Rubio <gerardmarquinarubio@gmail.com>
2022-12-08 16:29:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 curl https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash && 
 brew tap oven-sh/bun &&
-brew install wget node yarn deno bun oh-my-posh pfetch && 
+brew install wget node deno bun yarn oh-my-posh pfetch htop ngrok && 
 brew install --cask rectangle iterm2 alt-tab google-chrome visual-studio-code firefox microsoft-edge docker insomnia &&
 echo -e 'if [ $TERM_PROGRAM != "Apple_Terminal" ]; then\n\teval "$(oh-my-posh init zsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/1_shell.omp.json')"\nfi' >> ~/.zshrc &&
-echo -e 'oh-my-posh font install' | bash &&
-echo -e 'All done!'
+echo -e 'All done! Remember to run "oh-my-posh font install"'
